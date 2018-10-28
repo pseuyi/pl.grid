c6fc09af8f2aebd7798ac12d7c58d9af5b19c892
pseuyi <pseuyi@gmail.com>
2018-10-28 17:42:35 -0400

    add usePostForm hook, add hooks folder

---
+++ src/hooks/index.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const API = process.env.REACT_APP_API;
+
+const usePostForm = (initialValues = {}) => {
+  const [values, setFormValue] = useState(initialValues);
+  const [isSubmitting, submit] = useState(false);
+  let response = {};
+
+  useEffect(
+    () => {
+      if (isSubmitting) {
+        const data = { post: values };
+
+        fetch(`${API}/posts`, {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        })
+          .then(res => res.json())
+          .then(res => {
+            response.post = res.post;
+            setFormValue({});
+          })
+          .catch(err => { response.error = err; })
+          .finally(() => submit(false));
+      }
+    },
+    [isSubmitting]
+  )
+
+  return { setFormValue, submit, response };
+}
+
+export { usePostForm };
